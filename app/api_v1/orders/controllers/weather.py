b9eb0825cc1d0572b39d23151971d822906ea669
F0rtis <dedanik12@gmail.com>
2023-10-27 15:39:02 +0300

    updated weather_controller

--- app/api_v1/orders/controllers/weather.py
+++ app/api_v1/orders/controllers/weather.py
@@ -3,8 +3,12 @@ from .weather_api import get_weather_by_cords
 
 def inspect_weather(latitude:str, longitude: str) -> bool:
     weather = get_weather_by_cords(latitude=latitude, longitude=longitude)
+
+    if weather == "forbidden":
+        return False
+
     temp_cond = int(weather['temp']) < -1 or int(weather['temp']) > 40
-    wind_cond = int(weather['wind']) > 25
+    wind_cond = int(weather['wind_speed']) > 25
 
     if temp_cond and wind_cond: return False
     
--- app/api_v1/orders/controllers/weather_api.py
+++ app/api_v1/orders/controllers/weather_api.py
@@ -1,11 +1,13 @@
-import json
 import requests as req
 from core.config import settings
-import asyncio
 
 
 def get_weather_by_cords(latitude:str, longitude:str):
     url_yandex = f'https://api.weather.yandex.ru/v2/forecast/?lat={latitude}&lon={longitude}&[lang=ru_RU]'
     yandex_req = req.get(url_yandex, headers={'X-Yandex-API-Key': settings.weather_token}).json()
+
+    if yandex_req['message'] == 'forbidden':
+        return "forbidden"
+
     return yandex_req["fact"]
     
