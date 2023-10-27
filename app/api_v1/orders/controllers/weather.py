c2272a0a50c2f692b07c961a7ebdbffa99e75932
F0rtis <dedanik12@gmail.com>
2023-10-27 16:10:19 +0300

    updated weather controller

--- app/api_v1/orders/controllers/weather.py
+++ app/api_v1/orders/controllers/weather.py
@@ -1,15 +1,20 @@
 from .weather_api import get_weather_by_cords
 
+def good_weather_state(state: str) -> bool:
+    validate_word = {"clear", "partly-cloudy", "overcast", "cloudy",
+                "drizzle",}
+    
+    if state in validate_word: return True
+
+    return False
+
 
 def inspect_weather(latitude:str, longitude: str) -> bool:
     weather = get_weather_by_cords(latitude=latitude, longitude=longitude)
 
-    if weather == "forbidden":
-        return False
-
     temp_cond = int(weather['temp']) < -1 or int(weather['temp']) > 40
     wind_cond = int(weather['wind_speed']) > 25
 
-    if temp_cond and wind_cond: return False
+    if temp_cond or wind_cond or not good_weather_state(weather['condition']): return False
     
     return True
--- app/api_v1/orders/controllers/weather_api.py
+++ app/api_v1/orders/controllers/weather_api.py
@@ -3,11 +3,8 @@ from core.config import settings
 
 
 def get_weather_by_cords(latitude:str, longitude:str):
-    url_yandex = f'https://api.weather.yandex.ru/v2/forecast/?lat={latitude}&lon={longitude}&[lang=ru_RU]'
-    yandex_req = req.get(url_yandex, headers={'X-Yandex-API-Key': settings.weather_token}).json()
-
-    if yandex_req['message'] == 'forbidden':
-        return "forbidden"
+    url_yandex = f'https://api.weather.yandex.ru/v2/informers/?lat={latitude}&lon={longitude}&[lang=ru_RU]'
+    yandex_req = req.get(url_yandex, headers={'X-Yandex-API-Key': '771c47c8-130d-4d35-b352-696502b21ff1'}).json()
 
     return yandex_req["fact"]
     
