265248596b5100595aa443d7cc0a32200dad065c
F0rtis <dedanik12@gmail.com>
2023-10-28 00:13:46 +0300

    updated privacy

--- app/api_v1/orders/views.py
+++ app/api_v1/orders/views.py
@@ -10,24 +10,24 @@ router = APIRouter()
 
 @router.post("/create_order", response_model=Order, status_code=status.HTTP_201_CREATED)
 async def create_order(order_to_create: Order, session: AsyncSession = Depends(db_driver.get_scoped_session), token: JWTHeader = Depends(JWTBearer())):
-    return await curd.create_order(session=session, order_to_create=order_to_create)#
+    return await curd.create_order(session=session, order_to_create=order_to_create)
 
 
 @router.delete("/delete_order", response_model=DroneLink)
 async def delete_order(order_delete: DroneLink, session: AsyncSession = Depends(db_driver.get_scoped_session), token: JWTHeader = Depends(JWTBearer())):
-    return await curd.delete_order(session=session, order_to_delete=order_delete)#
+    return await curd.delete_order(session=session, order_to_delete=order_delete)
 
 
 @router.post("/run_order", response_model=DroneLink)
 async def run_order(order_to_push: DroneLink, session: AsyncSession = Depends(db_driver.get_scoped_session), token: JWTHeader = Depends(JWTBearer())):
-    return await curd.push_to_dron_state(session=session, order_to_push=order_to_push)#
+    return await curd.push_to_dron_state(session=session, order_to_push=order_to_push)
 
 
 @router.get("/get_all_orders", response_model=list[Order], status_code=status.HTTP_200_OK)
-async def get_all_orders(session: AsyncSession = Depends(db_driver.get_scoped_session)):
+async def get_all_orders(session: AsyncSession = Depends(db_driver.get_scoped_session), token: JWTHeader = Depends(JWTBearer())):
     return await curd.get_all_orders(session=session)
 
 
 @router.get("/get/{order_id}", status_code=status.HTTP_200_OK)
-async def get_order_by_id(order_id: str, session: AsyncSession = Depends(db_driver.get_scoped_session)):
+async def get_order_by_id(order_id: str, session: AsyncSession = Depends(db_driver.get_scoped_session), token: JWTHeader = Depends(JWTBearer())):
     return await curd.get_order_by_id(order_id=order_id, session=session)
